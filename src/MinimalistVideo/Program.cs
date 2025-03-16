33120b6a5c47a385a4c29c663e8d673a802a7934
Jerome Bell <jeromebell@gmail.com>
2025-03-16 12:12:03 -0500

    Revert "Prerendering (#21)"
    This reverts commit d9d53e1690ec830f1a263a1c1e1166588c0bb158.

--- src/MinimalistVideo/Program.cs
+++ src/MinimalistVideo/Program.cs
@@ -1,25 +1,16 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MinimalistVideo;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-if (!builder.RootComponents.Any())
-{
-    builder.RootComponents.Add<App>("#app");
-    builder.RootComponents.Add<HeadOutlet>("head::after");
-}
+builder.RootComponents.Add<MinimalistVideo.App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
-
-static void ConfigureServices(IServiceCollection services, string baseAddress)
-{
-    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
-
-    services
-        .AddMudServices()
-        .AddBlazoredLocalStorage();
-}
--- src/MinimalistVideo/Types.cs
+++
@@ -1,5 +0,0 @@
-﻿using ValueOf;
-
-public class Pixel : ValueOf<int, Pixel>;
-
-public class Percentage : ValueOf<decimal, Percentage>;
