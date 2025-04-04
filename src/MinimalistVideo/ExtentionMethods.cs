b5d430ffa9c99171fb1c0a81e010e39e5d8712f0
Jerome Bell <jeromebell@gmail.com>
2025-04-04 11:25:27 -0700

    Vimeo Start Time (#1)

---
+++ src/MinimalistVideo/ExtentionMethods.cs
@@ -0,0 +1,11 @@
+﻿using OneOf.Types;
+using OneOf;
+
+namespace MinimalistVideo;
+
+public static class ExtensionMethods
+{
+    public static OneOf<T, None> ToOption<T>(this T? value) where T : struct => value.HasValue ? value.Value : new None();
+
+    public static OneOf<string, None> ToOption(this string? value) => value != null ? value : new None();
+}
--- src/MinimalistVideo/Types.cs
+++ src/MinimalistVideo/Types.cs
@@ -1,5 +1,12 @@
-﻿using ValueOf;
+﻿using OneOf.Types;
+using OneOf;
+using ValueOf;
 
 public class Pixel : ValueOf<int, Pixel>;
 
 public class Percentage : ValueOf<decimal, Percentage>;
+
+/// <summary>
+///     Hash - Required for private Vimeo videos. See https://www.drupal.org/project/video_embed_field/issues/3238136
+/// </summary>
+public record Vimeo(string VideoId, OneOf<string, None> Hash);
